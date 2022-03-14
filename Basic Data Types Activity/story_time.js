a5c3762c7cc4dcc6600854ce6af58516b5ce73e4
norasyeezys <39389570+norasyeezys@users.noreply.github.com>
2022-03-13 21:43:45 -0400

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,28 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hello everyone!";
+let name = "Andrew";
+let location = "Mars";
+let title = "Hunting for Rare Artifacts";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 2035;
+let century = 21;
+let day = 144;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let valuables = ["Old Canteen", "Shiny Metal Rock", "Motherboard"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let foundValuable = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + "My name is "+ name + " and I'm from " + location + "!");
+console.log("The year is " + year + ", we are in century #" + century + ".");
+console.log("This is day " + day + " of the " + title + " challenge. ");
+console.log(foundValuable + ", yes, I have found some rare commodities!")
+console.log("Some of the things I found include: "+valuables[0]+", "+valuables[1]+", and "+valuables[2]);
